3eb5f2ebcba0cddfbb71ba086a9ae87c54b0be65
Francisco <franciscocolatinodelima@gmail.com>
2025-01-05 22:05:47 -0300

    fix: sidekiq when has redis url

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -28,17 +28,37 @@ if Rails.env.production? || Rails.env.staging?
 end
 
 if Rails.env.development?
+  uri = ENV['REDIS_URL'] || 'redis://localhost:6379/'
+
   Sidekiq.configure_server do |config|
-    config.redis = {
-      host: ENV['REDIS_HOST'],
-      port: ENV['REDIS_PORT'] || '6379'
-    }
+    if ENV['REDIS_HOST'].present?
+      config.redis = {
+        host: ENV['REDIS_HOST'],
+        port: ENV['REDIS_PORT'] || '6379'
+      }
+    else
+      config.redis = {
+        url: uri,
+        ssl_params: {
+          verify_mode: OpenSSL::SSL::VERIFY_NONE
+        }
+      }
+    end
   end
-  
+
   Sidekiq.configure_client do |config|
-    config.redis = {
-      host: ENV['REDIS_HOST'],
-      port: ENV['REDIS_PORT'] || '6379'
-    }
+    if ENV['REDIS_HOST'].present?
+      config.redis = {
+        host: ENV['REDIS_HOST'],
+        port: ENV['REDIS_PORT'] || '6379'
+      }
+    else
+      config.redis = {
+        url: uri,
+        ssl_params: {
+          verify_mode: OpenSSL::SSL::VERIFY_NONE
+        }
+      }
+    end
   end
 end
